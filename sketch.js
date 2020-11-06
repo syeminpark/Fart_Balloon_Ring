cf7280627b829729b5356aa313643de075beface
syeminpark <70131115+syeminpark@users.noreply.github.com>
2020-11-06 21:49:26 +0900

    left fart=left fart bubbles
    왼쪽 방귀 그림을 그리면, 왼쪽에서 방귀방울이 나오도록 코딩하였다. + 기존의 ellipse 모양들을 circle의 형태로 radius가 하나의 값을 가지게 만들어 dist()를 이용해서 범위를 구했다. 하드 코딩 방식으로 진행해서 아쉽지만..방귀 모양자체가 임의의 값들을 넣어서 만든 방식이라 어절 수 없었다. cos,sin을 이용해서 수학적 알고리즘을 이용해서 그림을 그릴 수 있는 방법또한 공부하였으나, 마찬가지로 그 그림 안에 포함되어있는지를 확인하는 것은 매우 어려워서 지금 작품에는 적용시킬 수 없을 것 같다는 결론에 도달하였다.

--- sketch.js
+++ sketch.js
@@ -76,14 +76,25 @@ function draw() {
 }
 
 function mousePressed() {
-
   bubbleNum = random(1, 5)
 
+  //left mask
+ if(dist(mouseX,mouseY,width/5,height*0.87)<50 ||dist(mouseX,mouseY,width/4.5,height*0.78)<30 || dist(mouseX,mouseY,width/6.4,height*0.818)<32
+    || dist(mouseX,mouseY,width/8,height*0.91)<30|| dist(mouseX,mouseY,width/5.05,height*0.93)<30|| dist(mouseX,mouseY,width/3.8,height*0.89)<30
+    || dist(mouseX,mouseY,width/3.8,height*0.82)<22.5|| dist(mouseX,mouseY,width/3.4,height*0.79)<22.5 || dist(mouseX,mouseY,width/3,height*0.79)<20){
   for(let i=0;i<bubbleNum;i++){
      rightBubble = createVector(random(width / 2.4, width / 3.3), random(height / 1.9, height / 2))
     let p= new fartBubble(rightBubble);
     FartBubble.push(p)
+  }
 }
+
+
+
+
+
+
+
 }
 
 class fartBubble {
@@ -227,44 +238,45 @@ class fart {
   }
   mask(){
     stroke(220)
-    strokeWeight(1)
+    strokeWeight(0.5)
     noFill()
 
    //left mask
 
-      ellipse(width/5,height*0.87,100,80)
+    ellipse(width/5,height*0.87,100)
 
-    ellipse(width/4.5,height*0.78,45,60)
+    ellipse(width/4.5,height*0.78,60)
 
-    ellipse(width/6.4,height*0.818,55,64)
-    ellipse(width/8,height*0.91,60,48)
-    ellipse(width/5.05,height*0.93,48,60)
-    ellipse(width/3.8,height*0.89,50,60)
-
-
-
-    ellipse(width/3.8,height*0.82,40,45)
-
-   ellipse(width/3.4,height*0.79,40,45)
-    ellipse(width/3,height*0.79,40,30)
-
-
-   //left mask
-    ellipse(width/5*4,height*0.87,100,80)
-
-    ellipse(width/4.5*3.5,height*0.78,45,60)
-
-    ellipse(width/6.4*5.4,height*0.818,55,64)
-    ellipse(width/8*7,height*0.91,60,48)
-    ellipse(width/5.05*4.05,height*0.93,48,60)
-    ellipse(width/3.8*2.8,height*0.89,50,60)
+    ellipse(width/6.4,height*0.818,64)
+    ellipse(width/8,height*0.91,60)
+    ellipse(width/5.05,height*0.93,60)
+    ellipse(width/3.8,height*0.89,60)
 
 
 
-    ellipse(width/3.8*2.8,height*0.82,40,45)
+    ellipse(width/3.8,height*0.82,45)
 
-   ellipse(width/3.4*2.4,height*0.79,40,45)
-    ellipse(width/3*2,height*0.79,40,30)
+   ellipse(width/3.4,height*0.79,45)
+    ellipse(width/3,height*0.79,40)
+
+
+   //rigt mask
+
+    ellipse(width/5*4,height*0.87,100)
+
+    ellipse(width/4.5*3.5,height*0.78,60)
+
+    ellipse(width/6.4*5.4,height*0.818,64)
+    ellipse(width/8*7,height*0.91,60)
+    ellipse(width/5.05*4.05,height*0.93,60)
+    ellipse(width/3.8*2.8,height*0.89,60)
+
+
+
+    ellipse(width/3.8*2.8,height*0.82,45)
+
+   ellipse(width/3.4*2.4,height*0.79,45)
+    ellipse(width/3*2,height*0.79,40)
   }
 }
 
